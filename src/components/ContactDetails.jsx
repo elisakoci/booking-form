d7bdaa1f11efe5e16793e8e38b3010c2724447bc
Elisa Omeri <elisa.omeri007@gmail.com>
2025-05-03 10:25:52 +0200

    completing ContactDetails component

--- src/components/ContactDetails.jsx
+++ src/components/ContactDetails.jsx
@@ -1,28 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+
+import validateFormField from "../helpers/validateFormField";
+import submitStep from "../helpers/submitStep";
+import CommonInput from "./CommonInput";
+import CommonButton from "./CommonButton";
 
 // Receive nextStep and updateFormData props
-const ContactDetails = (
-  {
-    // ...
-  }
-) => {
+const ContactDetails = ({ nextStep, updateFormData }) => {
   // Create a formData state
   // It should contain the firstName, lastName, email, and phone fields
-  // ...
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
   // Create an errors state
   // It should contain the firstName, lastName, email, and phone fields
-  // ...
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
 
   // Define the handleFormChange function
   // It should update the formData state with the new value
   // It should also update the errors state with the result of the validateFormField helper function
   // The validateFormField function should be called with the right arguments
-  // ...
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+
+    setErrors((prevErros) => ({
+      ...prevErros,
+      [name]: validateFormField(name, value),
+    }));
+  };
 
   // Define the submitContactDetails function
   // It should prevent the default form submission behavior
   // Then, it should call the submitStep helper function with the right arguments
-  // ...
+  const submitContactDetails = (e) => {
+    e.preventDefault();
+
+    submitStep({
+      formData,
+      errors,
+      setErrors,
+      onSuccess: () => {
+        updateFormData(1, formData);
+        nextStep();
+      },
+    });
+  };
 
   return (
     <div className="form-step contact-details">
@@ -36,7 +71,16 @@ const ContactDetails = (
         {/* It should display the value of the firstName field */}
         {/* It should call the handleFormChange function when the value changes */}
         {/* It should display the error for the firstName field */}
-        {/* ... */}
+        <CommonInput
+          required
+          id="firstName"
+          type="text"
+          name="firstName"
+          label="First Name"
+          value={formData.firstName}
+          onChange={handleFormChange}
+          error={errors.firstName}
+        />
         {/* Render a CommonInput component for the last name field */}
         {/* It should be required */}
         {/* It should have an id of "lastName" */}
@@ -46,7 +90,16 @@ const ContactDetails = (
         {/* It should display the value of the lastName field */}
         {/* It should call the handleFormChange function when the value changes */}
         {/* It should display the error for the lastName field */}
-        {/* ... */}
+        <CommonInput
+          required
+          id="lastName"
+          type="text"
+          name="lastName"
+          label="Last Name"
+          value={formData.lastName}
+          onChange={handleFormChange}
+          error={errors.lastName}
+        />
         {/* Render a CommonInput component for the email field */}
         {/* It should be required */}
         {/* It should have an id of "email" */}
@@ -56,7 +109,16 @@ const ContactDetails = (
         {/* It should display the value of the email field */}
         {/* It should call the handleFormChange function when the value changes */}
         {/* It should display the error for the email field */}
-        {/* ... */}
+        <CommonInput
+          required
+          id="email"
+          type="email"
+          name="email"
+          label="Email"
+          value={formData.email}
+          onChange={handleFormChange}
+          error={errors.email}
+        />
         {/* Render a CommonInput component for the phone field */}
         {/* It should be required */}
         {/* It should have an id of "phone" */}
@@ -66,11 +128,20 @@ const ContactDetails = (
         {/* It should display the value of the phone field */}
         {/* It should call the handleFormChange function when the value changes */}
         {/* It should display the error for the phone field */}
-        {/* ... */}
+        <CommonInput
+          required
+          id="phone"
+          type="tel"
+          name="phone"
+          label="Phone Number"
+          value={formData.phone}
+          onChange={handleFormChange}
+          error={errors.phone}
+        />
         {/* Render a CommonButton component */}
         {/* It should have a type of "submit" */}
         {/* It should have a text of "Next" */}
-        {/* ... */}
+        <CommonButton type="submit" text="Next" />
       </form>
     </div>
   );
